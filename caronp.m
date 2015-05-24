f14a30c4049f2181b71117d044cc61592d3b05a2
David ödling <davidodling@me.com>
2015-05-24 16:29:24 +0200

    caron description 	modified:   caronp.m

--- caronp.m
+++ caronp.m
@@ -1,4 +1,7 @@
 function matrix = caronp(mat,p)
+    % implementation of caron p transform.
+    % Caron, J Experiments with LSA scoring: Optimal rank and basis. 
+    % In: Berry, MW eds. (2001) Computational information retrieval. SIAM, Philadelphia, PA, pp. 157-169
     dim = min(size(mat));
     res = zeros(1,dim);
     for i=1:dim
